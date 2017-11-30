b93a75a33a5f84919e4912fa09813a3a5d0ca8a4
Shank2358 <33946139+Shank2358@users.noreply.github.com>
2017-11-30 14:36:49 +0800

    csv_create1.0

---
+++ csv_create/images&labels2csv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: UTF-8
+import sys
+import os.path
+
+os.system("E:")
+os.system("cd visual studio 2017/Target Intelligent Recognition/Datasets")
+os.system("dir /b/s *.pgm *.jpg >at.txt")
+
+if __name__ == "__main__":
+
+    # if len(sys.argv) != 2:
+    #    print "usage: create_csv <base_path>"
+    #    sys.exit(1)
+    # BASE_PATH=sys.argv[1]
+    BASE_PATH = "E:/visual studio 2017/Target Intelligent Recognition/Datasets"
+    SEPARATOR = ";"
+    fh = open("E:/visual studio 2017/Target Intelligent Recognition/Datasets/at.txt", 'w')
+
+    label = 1
+    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                abs_path = "%s/%s" % (subject_path, filename)
+                #print
+                #"%s%s%d" % (abs_path, SEPARATOR, label)
+                fh.write(abs_path)
+                fh.write(SEPARATOR)
+                fh.write(str(label))
+                fh.write("\n")
+            label = label + 1
+    fh.close()
